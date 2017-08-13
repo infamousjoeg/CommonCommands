04594c64cea58563c5cdd672fb1967e83dfccc00
Joe Garcia, CISSP <joe.garcia@cyberark.com>
2017-08-12 22:15:40 -0400

    Moved XAML to xaml.xml

--- Functions/Load-XAMLForm.ps1
+++ Functions/Load-XAMLForm.ps1
@@ -5,47 +5,25 @@ Converts XAML for PowerShell and loads XAML form
 .DESCRIPTION
 Converts Visual Studio WPF Application in Visual C# XAML into a form that is
 acceptable for PowerShell to use the XAML-based form
-.PARAMETER XAML
-The XAML from Visual Studio
 .EXAMPLE
-## Replace XAML between $inputXAML = @" "@
-$inputXAML = @"
-    <Window x:Class="Azure.Window1"
-            xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-            xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-            xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
-            xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
-            xmlns:local="clr-namespace:Azure"
-            mc:Ignorable="d"
-    
-            Title="iVision Azure Accelerator" Height="524.256" Width="332.076">
-        <Grid Margin="0,0,174,0">
-        </Grid>
-    </Window>
-"@
-
-$formVars = Load-XAMLForm $inputXAML
+$formVars = Load-XAMLForm
 .INPUTS
-Param
+Loads xaml.xml file from working directory
 .OUTPUTS
 Array of form variables found
 .NOTES
-TODO: Add pipeline support
 .LINK
 #>
     [CmdletBinding()]
     Param
-    (
-        [Parameter(Mandatory=$true)]
-        [string]
-        $XAML
-    )
+    ()
 
     BEGIN {}#begin
 
     PROCESS {
 
-        $parsedXML = $XAML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window'
+        $importedXML = Get-Content "${pwd}\xaml.xml"
+        $parsedXML = $importedXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window'
 
         [void][System.Reflection.Assembly]::LoadWithPartialName('PresentationFramework')
         Remove-Variable $XAML
@@ -61,8 +39,10 @@ TODO: Add pipeline support
                 "is installed." -ForegroundColor Red
         }
 
-        # Load XAML Objects in PowerShell
-        $XAML.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $Form.FindName($_.Name)}
+        # Set variables for each object
+        $XAML.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]")  | ForEach-Object {
+            New-Variable  -Name $_.Name -Value $Window.FindName($_.Name) -Force
+        } 
 
         $formVariables = @()
         $formVariables = Get-Variable WPF*
